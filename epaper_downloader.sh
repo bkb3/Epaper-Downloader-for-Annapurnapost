e7631b76a26a297f8e7662cb1202f8236e4b6c4c
Bikash Kumar Bhandari <mk.bikash@gmail.com>
2017-08-15 08:50:07 +0545

    Added more verbosity

--- epaper_downloader.sh
+++ epaper_downloader.sh
@@ -2,12 +2,13 @@
 
 #Before I start I will say that this is an ugly approach but still works.
 #Also you can append all the "rm -f" lines at the bottom if it confuses you in the middle. It is just to clean the unwanted files after their job is done.
+#Needs GNU Wget 1.16 or higher for progressbar options.
 
 
-echo "Downloading webpage"
+notify-send "Downloading homepage"
 #Download webpage
 wget "http://annapurnapost.com/epaper" -O /tmp/index.html -q 
-echo "Done"
+notify-send "Searching for epaper link"
 #We wanna grab the line <a href="http://annapurnapost.com/epaper/detail/465" title="Epaper Details"> which contains today's epaper link.
 paper=$(cat /tmp/index.html | grep -E 'Epaper.*Details');
 #remove the downloaded index file
@@ -19,9 +20,11 @@ link=$(cat /tmp/link.txt |grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*");
 
 rm -f /tmp/link.txt
 #Download the webpage of above captured link. It has all the images!
-echo "Finding links"
+#echo "The epaper is of the following date" 
+notify-send "Downloading epaper webpage"
 wget "$link" -O /tmp/epaper.html -q
 #Search for links
+notify-send "Searching for epaper images link"
 papers=$(cat /tmp/epaper.html | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
 rm -f /tmp/epaper.html
 echo "$papers" > /tmp/papers.txt
@@ -30,7 +33,7 @@ imglink=$(cat /tmp/papers.txt | grep "epaper/2017")
 rm -f /tmp/papers.txt
 #Output the found URLs to a file which is used by wget to download the images in the given directory.
 echo "$imglink" > /tmp/imglink.txt
-echo "Dowloading epapers"
-wget -i /tmp/imglink.txt -P "$HOME/Desktop/AnnapurnaPost" -q
+notify-send "Links found. Now dowloading epapers"
+wget -i /tmp/imglink.txt -P "$HOME/Desktop/AnnapurnaPost" -q --show-progress
 rm -f /tmp/imglink.txt
-echo "Done. Check the folder AnnapurnaPost in your desktop!"
+notify-send "Done. Check the folder AnnapurnaPost in your desktop."
